1346ff29c77b79e261570c3c7cbb287c0a0167c9
Webb Peng <webb.png@gmail.com>
2018-11-24 22:49:15 +0800

    optimize WechatAPI error hint

--- src/withWechatAPI.js
+++ src/withWechatAPI.js
@@ -1,9 +1,14 @@
 import React, { PureComponent } from 'react';
 import WechatAPIContext from './WechatAPIContext';
+import invariant from 'tiny-invariant';
 
 export default function withWechatAPI(WrappedComponent) {
 	return class WechatAPIConsumer extends PureComponent {
 		renderChildren = (wechatAPI) => {
+			invariant(
+				wechatAPI,
+				'You should not use <AppTitle> outside <WechatAPIProvider>',
+			);
 			return <WrappedComponent {...this.props} wechatAPI={wechatAPI} />;
 		};
 
